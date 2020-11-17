ce0d5a8a4b079ca991f5a5bce8d2e7de4b65f525
Leo Nistor <leo.nistor@gmail.com>
2020-11-17 10:39:17 +0200

    get storage: persistent todos

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ https://github.com/carnevalli/getx_todo_app_tutorial
 
 ## skinny flutter repo
 
-Nu `ios` and `android` folders. Use `flutter create .`.
+No `ios` and `android` folders. Use `flutter create .`.
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:getx_todo/screens/HomeScreen.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init();
+
   runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
